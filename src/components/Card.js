8f8d1a71a191ee6b26a7f0a4a8f5ae738897b392
Gaurav Vasudev <gaurav.nv1@gmail.com>
2018-08-19 12:36:02 -0700

    Tutorial Project for reference

---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// I can also just write { name, email, id } => {..} instead of props parameter
+const Card = (props) => {
+    const { name, email, id } = props;
+    return (
+        <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+            <img alt='robot' src={`https://robohash.org/${id}?200x200`} />
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
